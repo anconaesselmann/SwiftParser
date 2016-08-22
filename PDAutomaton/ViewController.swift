ea0c002f6f8b4ad3862e09dc4295c004334e6708
anconaesselmann <axel@anconaesselmann.com>
2016-08-21 23:39:31 -0700

    Created simple example program

--- PDAutomaton/ViewController.swift
+++ PDAutomaton/ViewController.swift
@@ -9,9 +9,15 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    @IBOutlet var textView: NSTextView!
+    @IBOutlet weak var regExPattern: NSTextField!
+    @IBOutlet weak var result: NSTextField!
+    
+    var re = RegEx()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        textView.textStorage?.append(NSAttributedString(string:"Phone: (626) 344-9785"))
 
         // Do any additional setup after loading the view.
     }
@@ -22,6 +28,19 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBAction func search(_ sender: AnyObject) {
+        let content = (textView.textStorage as NSAttributedString!).string
+        
+        re.pattern = regExPattern.stringValue
+        if let match = re.match(subject: content) {
+            let startIndex = content.index(content.startIndex, offsetBy: match)
+            let endIndex   = content.index(startIndex, offsetBy: re.matchLength)
+            let subString = content.substring(with: startIndex..<endIndex)
+            print(subString)
+            result.stringValue = subString
+        }
+        
+    }
 
 }
 
