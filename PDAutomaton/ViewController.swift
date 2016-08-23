7d87885d473132d428296554d0e997238aab6a32
anconaesselmann <axel@anconaesselmann.com>
2016-08-23 00:01:02 -0700

    Created timer for mac osx demo

--- PDAutomaton/ViewController.swift
+++ PDAutomaton/ViewController.swift
@@ -12,34 +12,47 @@ class ViewController: NSViewController {
     @IBOutlet var textView: NSTextView!
     @IBOutlet weak var regExPattern: NSTextField!
     @IBOutlet weak var result: NSTextField!
+    @IBOutlet weak var matchLabel: NSTextField!
+    @IBOutlet weak var matchStatus: NSTextField!
+    
+    var currentExpressionMatchingFunction:((String,String) -> String?)!
+
     
     var re = RegEx()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        matchLabel.stringValue = ""
+        matchStatus.stringValue = ""
+        currentExpressionMatchingFunction = matchUsingMyRegExEngine
         textView.textStorage?.append(NSAttributedString(string:"Phone: (626) 344-9785"))
-
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
 
     @IBAction func search(_ sender: AnyObject) {
         let content = (textView.textStorage as NSAttributedString!).string
         
-        re.pattern = regExPattern.stringValue
+        let startTime = CFAbsoluteTimeGetCurrent()
+        if let match = currentExpressionMatchingFunction(regExPattern.stringValue, content) {
+            result.stringValue = match
+            matchStatus.stringValue = "match found"
+        } else {
+            result.stringValue = ""
+            matchStatus.stringValue = "no match"
+        }
+        let timeElapsed = round((CFAbsoluteTimeGetCurrent() - startTime) * 1000) / 1000
+        matchLabel.stringValue = "Time: \(timeElapsed) seconds"
+    }
+    
+    func matchUsingMyRegExEngine(expression:String, content: String) -> String? {
+        re.pattern = expression
         if let match = re.match(subject: content) {
             let startIndex = content.index(content.startIndex, offsetBy: match)
             let endIndex   = content.index(startIndex, offsetBy: re.matchLength)
-            let subString = content.substring(with: startIndex..<endIndex)
+            let subString  = content.substring(with: startIndex..<endIndex)
             print(subString)
-            result.stringValue = subString
+            return subString
         }
-        
+        return nil
     }
 
 }
